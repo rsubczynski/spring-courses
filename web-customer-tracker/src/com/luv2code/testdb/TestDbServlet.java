977e4adbeeb29c959720966ebce0c7f44a46d6ba
Radosław Subczyński <radoslaw.subczynski@likims.com>
2019-03-27 10:45:47 +0100

    video 265

--- web-customer-tracker/src/com/luv2code/testdb/TestDbServlet.java
+++ web-customer-tracker/src/com/luv2code/testdb/TestDbServlet.java
@@ -1,6 +1,10 @@
 package com.luv2code.testdb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,11 +19,33 @@ public class TestDbServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
+		// setup connection variables
+
+		String user = "root";
+		String pass = "";
+
+		// get connection to database
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+		String driver = "com.mysql.jdbc.Driver";
+		try {
+			PrintWriter out = response.getWriter();
+
+			out.println(driver);
+
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+			out.println("Success");
+
+			myConn.close();
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			throw new ServletException(exc);
+		}
+	}
 }
