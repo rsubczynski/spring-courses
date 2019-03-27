67db77d2e53ca4dcc891b52deb662b2c6d041230
Radosław Subczyński <radoslaw.subczynski@likims.com>
2019-03-27 12:21:24 +0100

    add libs to app

--- web-customer-tracker/src/com/luv2code/testdb/TestDbServlet.java
+++ web-customer-tracker/src/com/luv2code/testdb/TestDbServlet.java
@@ -2,14 +2,14 @@ package com.luv2code.testdb;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
 
 /**
  * Servlet implementation class TestDbServlet
@@ -19,33 +19,45 @@ public class TestDbServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		// setup connection variables
-
-		String user = "root";
-		String pass = "";
-
-		// get connection to database
+		String user = "springstudent";
+		String pass = "springstudent";
+		
 		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
-		String driver = "com.mysql.jdbc.Driver";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		
+		// get connection to database
 		try {
 			PrintWriter out = response.getWriter();
-
-			out.println(driver);
-
+			
+			out.println("Connecting to database: " + jdbcUrl);
+			
+			Class.forName(driver);
+			
 			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
-
-			out.println("Success");
-
+			
+			out.println("SUCCESS!!!");
+			
 			myConn.close();
-		} catch (Exception exc) {
+			
+		}
+		catch (Exception exc) {
 			exc.printStackTrace();
 			throw new ServletException(exc);
 		}
+	
+	
 	}
+
 }
+
+
+
+
+
+
+
