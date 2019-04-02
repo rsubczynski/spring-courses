a7ac5e2d4150c130b2da78a62d77a3dbe8dc1d3b
Radoslaw Subczynski <kozak221@gmail.com>
2019-04-02 22:04:36 +0200

    video 322

---
+++ spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,27 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
+		Account myAccount = new Account();
+		boolean vipFlag = true;
+		theAccountDAO.addAccount(myAccount, vipFlag);
+		theAccountDAO.doWork();
+
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.doToSleep();
+		context.close();
+	}
+
+}
