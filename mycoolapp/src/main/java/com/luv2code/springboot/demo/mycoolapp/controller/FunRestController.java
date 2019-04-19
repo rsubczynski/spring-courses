fb2bb09b42bac5fc96db5339983a4afc52b2d05e
Radosław Subczyński <radoslaw.subczynski@likims.com>
2019-04-19 15:39:42 +0200

    video 514

---
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/controller/FunRestController.java
@@ -0,0 +1,29 @@
+package com.luv2code.springboot.demo.mycoolapp.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World! Time on server is " + LocalDateTime.now();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
